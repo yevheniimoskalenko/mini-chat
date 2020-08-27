907f5686f16a62669a2ebcbd1382443ae8186cc6
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-08-27 15:05:30 +0300

    send all users message and add typing in console

--- index.js
+++ index.js
@@ -16,6 +16,24 @@ app.use('/', (req,res)=> {
 
 io.on('connection', soket => {
   console.log('new connect ')
+  soket.on('change_name', data =>{
+    soket.username = data
+    soket.emit('closeForm', {form: true})
+  })
+  soket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  soket.on('send_message', data => {
+    const messaage = {
+      message: data.message,
+      username: soket.username,
+      className: data.className
+    }
+    io.sockets.emit('add_message', messaage)
+  })
+  soket.on('typing', (data) => {
+    soket.broadcast.emit('typing', {username : soket.username})
+  })
 })
 
 
--- public/js/common.js
+++ public/js/common.js
@@ -1,11 +1,47 @@
 const connect = document.querySelector('.connect')
 const socket = io.connect("http://localhost:3000");
-console.log(socket)
-// const name = 
+const name = document.querySelector('.name')
+const form = document.querySelector('.auth')
+const sendBTN = document.getElementById('btnSend')
+const message = document.getElementById('message')
+const formMessage = document.querySelector('.message')
+const addMessage = document.querySelector('.allMessage')
+socket.on('closeForm', data => {
+if(data.form === true){
+  form.style.display = 'none'
+  formMessage.style.display = 'block'
+}
+})
 
-connect.addEventListener('click', (event)=> {
+socket.on('add_message', data => {
+ addMessage.innerHTML += `<div class="chatMessage">
+ <span>${data.username}</span>
+ <p>${data.message}</p>
+ </div>
+ `
+})
+
+sendBTN.addEventListener('click',(event) =>{
   event.preventDefault()
-  
+  const formSend = {
+    message: message.value,
+    className: 'my'
+  }
+  socket.emit('send_message', formSend)
+})
 
+
+message.addEventListener('keypress', ()=>{
+  socket.emit("typing");
   
+})
+
+
+
+socket.on("typing", data => {
+ console.log('tiping ',data.username)
+});
+connect.addEventListener('click', (event)=> {
+  event.preventDefault() 
+  socket.emit('change_name', name.value) 
 })
